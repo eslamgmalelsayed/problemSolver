6910889cb7ecb43a52702dd308728e8a86b62bb4
eslamgmalelsayed <eslamgmal1@gmail.com>
2024-09-20 16:14:44 +0300

    done

---
+++ DAY1.js
@@ -0,0 +1,35 @@
+// FizzBuzz
+const FizzBuzz = (n) => {
+    for (let i = 0; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FIZZBUZZ");
+      } else if (i % 3 === 0) {
+        console.log("FIZZ");
+      } else if (i % 5 === 0) {
+        console.log("BUZZ");
+      } else {
+        console.log(`${i} is not valid num`);
+      }
+    }
+  };
+  
+  // FizzBuzz(100);
+  
+  // reverse string
+  const reverseStr = (str) => {
+    console.log(str.split("").reverse().join(""));
+  };
+  
+  // reverseStr("hello");
+  
+  // Count Vowels
+  const CountVowels = (str) => {
+    let count = 0;
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i].match(/[aeiouAEIOU]/g)) count++;
+    }
+    console.log(count);
+  };
+  
+  // CountVowels("Hello World");
