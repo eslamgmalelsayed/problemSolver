a51e5175e6e8bd3cdda575e8e4b2d7ba2d514e7a
eslamgmalelsayed <eslamgmal1@gmail.com>
2024-09-20 19:17:06 +0300

    Update DAY1.js

--- DAY1.js
+++ DAY1.js
@@ -25,7 +25,6 @@ const FizzBuzz = (n) => {
   // Count Vowels
   const CountVowels = (str) => {
     let count = 0;
-  
     for (let i = 0; i < str.length; i++) {
       if (str[i].match(/[aeiouAEIOU]/g)) count++;
     }
