71057ffed7b118e4fc8266819bb94bdc42844704
eslamgmalelsayed <eslamgmal1@gmail.com>
2024-09-21 12:46:15 +0300

    done

--- DAY2.js
+++ DAY2.js
@@ -1,1 +1,24 @@
-// start
+// Merge Two Sorted Arrays
+const mergeSortedArr = (arr1, arr2) => {
+  let newArr = [...arr1, ...arr2].sort();
+  console.log(newArr);
+};
+
+// mergeSortedArr([1, 3, 5],[2, 4, 6]);
+
+// Palindrome Check
+const Palindrome = (str) => {
+  let handleStr = str.split("").join("").toLowerCase().replace(/\s/g, "");
+  let reversedStr = handleStr.split(" ").reverse().join("");
+  console.log(handleStr === reversedStr);
+};
+
+// Palindrome("A man a plan a canal Panama");
+
+// Find the Longest Word
+const FindLongestWord = (word) => {
+    let handleWord = word.split(' ').reduce((acc, curr) => acc.length > curr.length ? acc : curr );
+    console.log(handleWord);
+  };
+  
+// FindLongestWord("The quick brown fox jumps over the lazy dog");
