c59fa6d588604d6f7de87e69c0076c49466c2e63
eslamgmalelsayed <eslamgmal1@gmail.com>
2024-09-22 15:46:22 +0300

    Update DAY2.js

--- DAY2.js
+++ DAY2.js
@@ -7,18 +7,18 @@ const mergeSortedArr = (arr1, arr2) => {
 // mergeSortedArr([1, 3, 5],[2, 4, 6]);
 
 // Palindrome Check
-const Palindrome = (str) => {
+const palindrome = (str) => {
   let handleStr = str.split("").join("").toLowerCase().replace(/\s/g, "");
   let reversedStr = handleStr.split(" ").reverse().join("");
   console.log(handleStr === reversedStr);
 };
 
-// Palindrome("A man a plan a canal Panama");
+// palindrome("A man a plan a canal Panama");
 
 // Find the Longest Word
-const FindLongestWord = (word) => {
+const findLongestWord = (word) => {
     let handleWord = word.split(' ').reduce((acc, curr) => acc.length > curr.length ? acc : curr );
     console.log(handleWord);
   };
   
-// FindLongestWord("The quick brown fox jumps over the lazy dog");
+// findLongestWord("The quick brown fox jumps over the lazy dog");
